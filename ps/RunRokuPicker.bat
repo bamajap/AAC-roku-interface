ad4031e15f4551b712a4b053a3dd8333450f0938
BamaJAP <22374001+bamajap@users.noreply.github.com>
2024-06-15 20:19:34 -0500

    Added Roku selector tool for changing devices on the fly

---
+++ ps/RunRokuPicker.bat
@@ -0,0 +1,1 @@
+powershell.exe -ExecutionPolicy Bypass -File "RokuPicker.ps1"
--- scripts/run_roku_command.cmd
+++ scripts/run_roku_command.cmd
@@ -1,2 +1,18 @@
 @echo off
+
+setlocal
+set pwd="%cd%"
+
+:: Setting the curl app's directory.
+cd ..
+set CURL_DIR="%cd%\curl-7.52.1"
+
+cd %pwd%
+
+for /f "usebackq tokens=*" %%i in (`type ..\ps\RokuIP.txt`) do (
+    set "ROKU_IP=%%i"
+)
+
 "%CURL_DIR%\src\curl.exe" -d '' http://%ROKU_IP%:8060/%1
+
+endlocal
